ccbb5d2a28af9ba85ae710a8faf7514a91572345
Van Huy <huyvk3110@gmail.com>
2019-09-29 10:05:51 +0700

    update

--- handle.js
+++ handle.js
@@ -79,13 +79,13 @@ module.exports = {
                                             this.sendMessage(message.sender.id, `Chính xác: ${math}`);
                                         } else {
                                             dataDB.gameStatus = type.GAME_STATUS.WAITING;
-                                            this.sendMessage(message.sender.id, `Chưa chính xác rồi\nBạn đã hoàn thành ${dataDB.gameData.length - 1} câu hỏi trong ${Math.round((new Date() - dataDB.gameTime)/1000)} giây\nGõ "sẵn sàng" để bắt đầu lại nha`);
+                                            this.sendMessage(message.sender.id, `Chưa chính xác rồi\nBạn đã hoàn thành ${dataDB.gameData.length - 1} câu hỏi trong ${Math.round((new Date().getTime() - dataDB.gameTime)/1000)} giây\nGõ "sẵn sàng" để bắt đầu lại nha`);
                                             dataDB.gameData = [];
                                         }
                                     } else {
                                         let math = this.randomMath(dataDB.gameData.length / 5 + 1);
                                         dataDB.gameData.push(math);
-                                        dataDB.gameTime = new Date();
+                                        dataDB.gameTime = new Date().getTime();
                                         this.sendMessage(message.sender.id, `Bắt đầu nào: ${math}`);
                                     }
                                 }
